6c71afcbbd386cd6ba1b4c0a05e0cf05dee3b11f
Jah003 <jahna.florine@gmail.com>
2021-04-22 09:59:45 +0200

    Correction of Serie with remarks of PR#14

--- src/main/java/dataframe/Serie.java
+++ src/main/java/dataframe/Serie.java
@@ -8,52 +8,35 @@ import java.lang.Class;
 public class Serie<T> implements Serial<T>{
 
 	private List<T> elements;
-	private boolean isNum;
-
-	private String class_name;
 
 
 	public Serie() {
 		elements = new ArrayList<T>();
-		isNum = false;	
+	
 	}
 
 	public boolean add(T e){
 		//TODO : add compatibility with heredity
 		if(elements.size()==0){
-			if(e instanceof Integer || e instanceof Float || e instanceof Double){
-				isNum = true;
-				elements.add(e);
-
-			}else if(e instanceof String){
+			if(e instanceof Integer || e instanceof Float || e instanceof Double || e instanceof String){
+			
 				elements.add(e);
 
 			}else{
-				throw new IllegalArgumentException("Ce type n'est pas pris en charge");
+				throw new IllegalArgumentException("Invalid type element");
 			}
-
-			class_name = e.getClass().getName();
-
 		}else{
-			String test = e.getClass().getName();
-
-			if(test.equals(class_name)){
-				elements.add(e);
-				return true;
-			}
+			elements.add(e);
+			return true;
+			
 		}
 		return false;
 	}
 
 
     public boolean remove(int index){
-    	if(index < 0 || index >= elements.size())
-    		throw new NullPointerException("L'index est incorrect");
-    	if(index >= 0 && index < elements.size()){
-    		elements.remove(index);
-    		return true;
-    	}
-    	return false;
+        T e = elements.remove(index);
+        return e != null;
     }
 
     public int size(){
@@ -69,25 +52,17 @@ public class Serie<T> implements Serial<T>{
     }
 
     public boolean set(T e, int index){
-    	String test = e.getClass().getName();
     	if(index < 0 || index >= elements.size())
-    		throw new NullPointerException("L'index est incorrect");
-    	if(test.equals(class_name)){
-	    	elements.remove(index);
-	    	elements.add(index,e);
-	    	return true;
-    	}
-    	return false;
+    		throw new NullPointerException("Invalid index");
+    	
+	    elements.remove(index);
+	    elements.add(index,e);
+	    return true;
     }
 
 
     public T get(int index){
-    	T r = null;
-    	if(index < 0 || index >= elements.size())
-    		throw new NullPointerException("L'index est incorrect");
-    	if(index >= 0 && index < elements.size())
-    		r = elements.get(index);
-    	return r;
+    	return elements.get(index);
     }
 
 }
