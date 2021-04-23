a32c7700ada01754d406629afd66d76893e4a8a1
Romsou <ro.soumard@laposte.net>
2021-04-23 22:12:28 +0200

    Basic implementation of Dataframe and typeinfering

---
+++ src/main/java/dataframe/DataFrame.java
@@ -0,0 +1,44 @@
+package dataframe;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class DataFrame {
+
+    private ArrayList<Object> frame;
+
+    public DataFrame(File csv) {
+        // To implement
+    }
+
+    public DataFrame(ArrayList<Object> array) {
+        // To implement
+    }
+
+    public void add(int column) {
+        // To implement
+    }
+
+    public void remove(int column, int row) {
+        // To implement
+    }
+
+    public void delete(int column, int row) {
+        // To implement
+    }
+
+    public Object sum(int column) {
+        // To implement
+        return null;
+    }
+
+    public Object min(int column) {
+        // To implement
+        return null;
+    }
+
+    public Object max(int column) {
+        // To implement
+        return null;
+    }
+}
