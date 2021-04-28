e83e59d871b56dce147339540f534e6cb333a02e
clerysemanaz <clery.sem@gmail.com>
2021-04-28 08:41:49 +0200

    cleanup code

--- src/test/java/dataframe/DataFrameTest.java
+++ src/test/java/dataframe/DataFrameTest.java
@@ -6,16 +6,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class DataFrameTest {
     private ArrayList<Object> doubleSerie;
@@ -27,9 +26,9 @@ class DataFrameTest {
     @BeforeEach
     void setUp() {
         //TODO: Activate tests once Serie has been coded.
-        this.doubleSerie = new ArrayList<Object>(Collections.nCopies(10,1.1));
-        this.stringSerie = new ArrayList<Object>(Collections.nCopies(10,"a"));
-        this.integerSerie = new ArrayList<Object>(Collections.nCopies(10,1));
+        this.doubleSerie = new ArrayList<Object>(Collections.nCopies(10, 1.1));
+        this.stringSerie = new ArrayList<Object>(Collections.nCopies(10, "a"));
+        this.integerSerie = new ArrayList<Object>(Collections.nCopies(10, 1));
         this.booleanSerie = new ArrayList<Object>(Collections.nCopies(10, false));
     }
 
@@ -42,13 +41,16 @@ class DataFrameTest {
     }
 
     @Test
-    void create() throws UnsupportedTypeException, EmptyArrayException, FileNotFoundException, ColumnSizeMissmatch {
+    void create() throws UnsupportedTypeException, EmptyArrayException, FileNotFoundException, ColumnSizeMismatch {
         DataFrame frame = new DataFrame(stringSerie, integerSerie, doubleSerie);
         assertEquals(3, frame.getNbColumn());
         assertEquals(10, frame.getNbLine());
 
         File f = new File("src/resources/addresses.csv");
         DataFrame frameCSV = new DataFrame(f);
+        assertEquals(6, frameCSV.getNbColumn());
+        assertEquals(5, frameCSV.getNbLine());
+
     }
 
     @Test
@@ -68,22 +70,22 @@ class DataFrameTest {
         });
 
 
-        Assertions.assertThrows(ColumnSizeMissmatch.class, () -> {
-            doubleSerie.add(doubleSerie.size(),0.2);
+        Assertions.assertThrows(ColumnSizeMismatch.class, () -> {
+            doubleSerie.add(doubleSerie.size(), 0.2);
             DataFrame frame = new DataFrame(integerSerie, doubleSerie);
             assertEquals(0, frame.getNbColumn());
             assertEquals(10, frame.getNbLine());
         });
 
-        Assertions.assertThrows(ColumnSizeMissmatch.class, () -> {
-            doubleSerie.add(doubleSerie.size(),0.2);
+        Assertions.assertThrows(ColumnSizeMismatch.class, () -> {
+            doubleSerie.add(doubleSerie.size(), 0.2);
             DataFrame frame = new DataFrame(integerSerie);
             frame.addColumn(doubleSerie);
             assertEquals(0, frame.getNbColumn());
             assertEquals(10, frame.getNbLine());
         });
 
-        Assertions.assertThrows(ColumnSizeMissmatch.class, () -> {
+        Assertions.assertThrows(ColumnSizeMismatch.class, () -> {
             File f = new File("src/resources/addressesWrong.csv");
             DataFrame frameCSV = new DataFrame(f);
         });
@@ -91,7 +93,7 @@ class DataFrameTest {
     }
 
     @Test
-    void add() throws UnsupportedTypeException, EmptyArrayException, ColumnSizeMissmatch {
+    void add() throws UnsupportedTypeException, EmptyArrayException, ColumnSizeMismatch {
         DataFrame frame = new DataFrame(stringSerie);
         assertEquals(1, frame.getNbColumn());
         assertEquals(10, frame.getNbLine());
@@ -108,7 +110,7 @@ class DataFrameTest {
     }
 
     @Test
-    void remove() throws UnsupportedTypeException, EmptyArrayException, ColumnSizeMissmatch {
+    void remove() throws UnsupportedTypeException, EmptyArrayException, ColumnSizeMismatch {
         DataFrame frame = new DataFrame(doubleSerie);
         assertEquals(1, frame.getNbColumn());
         assertEquals(10, frame.getNbLine());
@@ -119,7 +121,7 @@ class DataFrameTest {
     }
 
     @Test
-    void max() throws UnsupportedTypeException, EmptyArrayException, EmptySerieException, ColumnSizeMissmatch {
+    void max() throws UnsupportedTypeException, EmptyArrayException, EmptySerieException, ColumnSizeMismatch {
         DataFrame frame = new DataFrame(integerSerie);
         assertEquals(1, frame.max(0));
 
@@ -142,7 +144,7 @@ class DataFrameTest {
     }
 
     @Test
-    void min() throws UnsupportedTypeException, EmptyArrayException, EmptySerieException, ColumnSizeMissmatch {
+    void min() throws UnsupportedTypeException, EmptyArrayException, EmptySerieException, ColumnSizeMismatch {
         DataFrame frame = new DataFrame(integerSerie);
         assertEquals(1, frame.min(0));
 
@@ -165,10 +167,10 @@ class DataFrameTest {
     }
 
     @Test
-    void sum() throws UnsupportedTypeException, EmptyArrayException, ColumnSizeMissmatch {
+    void sum() throws UnsupportedTypeException, EmptyArrayException, ColumnSizeMismatch {
         DataFrame frame = new DataFrame(integerSerie, doubleSerie);
         assertEquals(10, frame.sum(0));
-        assertEquals(11.0, (double)frame.sum(1), 1e-10);
+        assertEquals(11.0, (double) frame.sum(1), 1e-10);
     }
 
     @Test
@@ -180,30 +182,30 @@ class DataFrameTest {
     }
 
     @Test
-    void print() throws UnsupportedTypeException, EmptyArrayException, FileNotFoundException, URISyntaxException, ColumnSizeMissmatch {
+    void print() throws UnsupportedTypeException, EmptyArrayException, FileNotFoundException, URISyntaxException, ColumnSizeMismatch {
         DataFrame frame = new DataFrame(stringSerie, integerSerie, doubleSerie);
-        assertEquals("0 a\t1\t1.1\t\n", frame.print(0,1));
-        assertEquals("0 a\t1\t1.1\t\n1 a\t1\t1.1\t\n2 a\t1\t1.1\t\n", frame.print(0,3));
-        assertEquals("3 a\t1\t1.1\t\n4 a\t1\t1.1\t\n", frame.print(3,5));
-        assertEquals("8 a\t1\t1.1\t\n9 a\t1\t1.1\t\n", frame.print(8,100));
-        assertEquals(null, frame.print(0,0));
-        assertEquals(null, frame.print(9,8));
+        assertEquals("0 a\t1\t1.1\t\n", frame.print(0, 1));
+        assertEquals("0 a\t1\t1.1\t\n1 a\t1\t1.1\t\n2 a\t1\t1.1\t\n", frame.print(0, 3));
+        assertEquals("3 a\t1\t1.1\t\n4 a\t1\t1.1\t\n", frame.print(3, 5));
+        assertEquals("8 a\t1\t1.1\t\n9 a\t1\t1.1\t\n", frame.print(8, 100));
+        assertNull(frame.print(0, 0));
+        assertNull(frame.print(9, 8));
 
         URL resource = getClass().getClassLoader().getResource("addresses.csv");
 
         File f = new File(resource.toURI());
         DataFrame frameCSV = new DataFrame(f);
-        assertEquals("0 John\tDoe\t120 jefferson st.\tRiverside\t NJ\t8075\t\n", frameCSV.print(0,1));
+        assertEquals("0 John\tDoe\t120 jefferson st.\tRiverside\t NJ\t8075\t\n", frameCSV.print(0, 1));
         assertEquals("2 John Da Man\tRepici\t120 Jefferson St.\tRiverside\t NJ\t8075\t\n" +
                 "3 Stephen\tTyler\t7452 Terrace At the Plaza road\tSomeTown\tSD\t91234\t\n" +
-                "4 NULL\tBlankman\tNULL\tSomeTown\t SD\t298\t\n", frameCSV.print(2,5));
-        assertEquals(null, frameCSV.print(0,0));
-        assertEquals(null, frameCSV.print(1,0));
+                "4 NULL\tBlankman\tNULL\tSomeTown\t SD\t298\t\n", frameCSV.print(2, 5));
+        assertNull(frameCSV.print(0, 0));
+        assertNull(frameCSV.print(1, 0));
 
     }
 
     @Test
-    void selectFromColumn() throws ColumnSizeMissmatch, EmptyArrayException, UnsupportedTypeException, EmptySerieException, ColumnNotFoundException {
+    void selectFromColumn() throws ColumnSizeMismatch, EmptyArrayException, UnsupportedTypeException, EmptySerieException, ColumnNotFoundException {
         DataFrame frame = new DataFrame(stringSerie, integerSerie, doubleSerie);
 
         ArrayList<String> columnsNames = new ArrayList<>();
@@ -229,18 +231,18 @@ class DataFrameTest {
     }
 
     @Test
-    void selectFromColumnException() throws ColumnSizeMissmatch, EmptyArrayException, UnsupportedTypeException {
+    void selectFromColumnException() throws ColumnSizeMismatch, EmptyArrayException, UnsupportedTypeException {
         DataFrame frame = new DataFrame(stringSerie, integerSerie, doubleSerie);
         ArrayList<String> columnNames = new ArrayList<>();
         columnNames.add("column1");
-        columnNames.add("column"+frame.getNbLine());
+        columnNames.add("column" + frame.getNbLine());
         Assertions.assertThrows(ColumnNotFoundException.class, () -> {
             DataFrame frame2 = frame.DataFrameFromColumns(columnNames);
         });
     }
 
     @Test
-    void selectFromLine() throws ColumnSizeMissmatch, EmptyArrayException, UnsupportedTypeException, EmptySerieException {
+    void selectFromLine() throws ColumnSizeMismatch, EmptyArrayException, UnsupportedTypeException, EmptySerieException {
         DataFrame frame = new DataFrame(stringSerie, integerSerie, doubleSerie);
 
         ArrayList<Integer> lineIndexes = new ArrayList<>();
@@ -270,7 +272,7 @@ class DataFrameTest {
     }
 
     @Test
-    void selectFromLineException() throws ColumnSizeMissmatch, EmptyArrayException, UnsupportedTypeException {
+    void selectFromLineException() throws ColumnSizeMismatch, EmptyArrayException, UnsupportedTypeException {
         DataFrame frame = new DataFrame(stringSerie, integerSerie, doubleSerie);
         ArrayList<Integer> lineIndexes = new ArrayList<>();
         lineIndexes.add(1);
